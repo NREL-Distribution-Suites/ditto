0071c6e95e5c45cf147e0c00c7adc8a475b8635f
Latif <aadil.latif@gmail.com>
2024-03-20 13:50:32 -0600

    test path update

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,7 +5,7 @@ import pytest
 
 from ditto.readers.opendss.reader import Reader
 
-OPENDSS_CASEFILES = (Path(__file__).parent / "opendss_models").rglob("Master.dss")
+OPENDSS_CASEFILES = (Path(__file__).parent / "data"/ "opendss_circuit_models").rglob("Master.dss")
 
 
 @pytest.mark.parametrize("opendss_file", OPENDSS_CASEFILES)
@@ -14,4 +14,7 @@ def test_serialize_model(opendss_file: Path, tmp_path):
     system = parser.get_system()
     json_path = Path(tmp_path) / (opendss_file.stem.lower() + ".json")
     system.to_json(json_path, overwrite=True)
+    print(json_path)
+    import os
+    os.system("pause")
     assert json_path.exists, "Failed to export the json file"

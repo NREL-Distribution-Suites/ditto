c83d14956818ba5e82a55b7e5dd2736a57a0fc30
Latif <aadil.latif@gmail.com>
2024-03-20 11:29:28 -0600

    develop cleanup

--- src/ditto/readers/reader.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from infrasys.system import System
-
-
-class Reader(ABC):
-    @abstractmethod
-    def get_system(self) -> System:
-        ...
--- tests/enumerations.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class OpenDSSTestModels(str, Enum):
-    FOUR_WIRE = "4wire-Delta-3"
-    IEEE13 = "ieee13"
-    P4U = "P4U"

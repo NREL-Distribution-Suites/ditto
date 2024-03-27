f751c6cb82db5d30fbfcdf9290abbfb525e4b431
telgindy <tarek.elgindy@nrel.gov>
2024-03-26 21:20:34 -0600

    adding initial abstract writer in base writer folder

---
+++ src/ditto/writers/abstract_writer.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+from infrasys.system import System
+
+
+class AbstractWriter(ABC):
+
+    def __init__(self, system: System):
+        self.system = system
+    
+    @abstractmethod
+    def write(self) -> None:
+        ...

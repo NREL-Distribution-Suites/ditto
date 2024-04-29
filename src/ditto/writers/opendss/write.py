297640c8bbd24807bdcd93d59c58e413e0ab883e
Kapil Duwadi <Kapil.Duwadi@nrel.gov>
2024-04-29 17:27:30 -0500

    Refactored tests
    - Added altdss_schema pkg
    - Refactored tests to use paramterize tests
    - Removed git submodule

--- src/ditto/writers/opendss/write.py
+++ src/ditto/writers/opendss/write.py
@@ -1,21 +1,34 @@
 from pathlib import Path
-import importlib
 from collections import defaultdict
+from typing import Any
+
+from altdss_schema import altdss_models
 
 from ditto.writers.abstract_writer import AbstractWriter
-from ditto.writers.opendss.components.distribution_bus import DistributionBusMapper
-from gdm import DistributionBus
-
-altdss_models = importlib.import_module("ditto.formats.opendss.AltDSS-Schema.python.altdss_models") 
 import ditto.writers.opendss as opendss_mapper
 
 
-
 class Writer(AbstractWriter):
+    def _get_dss_string(self, model_map: Any) -> str:
+        # Example model_map is instance of DistributionBusMapper
+        altdss_class = getattr(altdss_models, model_map.altdss_name)
+        # Example altdss_class is Bus
+        altdss_object = altdss_class.model_validate(model_map.opendss_dict)
+        if model_map.altdss_composition_name is not None:
+            altdss_composition_class = getattr(altdss_models, model_map.altdss_composition_name)
+            altdss_composition_object = altdss_composition_class(altdss_object)
+            dss_string = altdss_composition_object.dumps_dss()
+        else:
+            dss_string = altdss_object.dumps_dss()
 
-    def write(self, output_path: Path = Path('./'), separate_substations: bool=True, separate_feeders: bool=True):
+        return dss_string
 
-
+    def write(
+        self,
+        output_path: Path = Path("./"),
+        separate_substations: bool = True,
+        separate_feeders: bool = True,
+    ):
         files_to_redirect = []
         feeders_redirect = defaultdict(list)
         substations_redirect = defaultdict(list)
@@ -24,64 +37,52 @@ class Writer(AbstractWriter):
         for component_type in component_types:
             # Example component_type is DistributionBus
             components = self.system.get_components(component_type)
-            mapper_name = component_type.__name__+"Mapper"
+            mapper_name = component_type.__name__ + "Mapper"
             # Example mapper_name is string DistributionBusMapper
-            if hasattr(opendss_mapper, mapper_name):
-                print(f'Mapping components in {mapper_name}...')
-                mapper = getattr(opendss_mapper, mapper_name)
-                # Example mapper is class DistributionBusMapper
+            if not hasattr(opendss_mapper, mapper_name):
+                print(f"Mapper {mapper_name} not found. Skipping")
+                continue
 
-                for model in components:
-                    # Example model is instance of DistributionBus
-                    model_map = mapper(model)
-                    # Example model_map is instance of DistributionBusMapper
-                    model_map.populate_opendss_dictionary()
-                    altdss_class = getattr(altdss_models, model_map.altdss_name)
-                    # Example altdss_class is Bus
-                    altdss_object = altdss_class.model_validate(model_map.opendss_dict)
-                    if model_map.altdss_composition_name is not None:
-                        altdss_composition_class = getattr(altdss_models, model_map.altdss_composition_name)
-                        altdss_composition_object = altdss_composition_class(altdss_object)
-                        dss_string = altdss_composition_object.dumps_dss()
-                    else:
-                        dss_string = altdss_object.dumps_dss()
+            print(f"Mapping components in {mapper_name}...")
+            mapper = getattr(opendss_mapper, mapper_name)
+            # Example mapper is class DistributionBusMapper
 
-                    output_folder = output_path
-                    output_redirect = Path("")
-                    if separate_substations:
-                        output_folder = Path(output_path,model_map.substation)
-                        output_redirect = Path(model_map.substation)
-                        output_folder.mkdir(exist_ok=True)
+            for model in components:
+                # Example model is instance of DistributionBus
+                model_map = mapper(model)
+                model_map.populate_opendss_dictionary()
+                dss_string = self._get_dss_string(model_map)
 
-                    else:
-                        output_folder.mkdir(exist_ok=True)
+                output_folder = output_path
+                output_redirect = Path("")
+                if separate_substations:
+                    output_folder = Path(output_path, model_map.substation)
+                    output_redirect = Path(model_map.substation)
+                    output_folder.mkdir(exist_ok=True)
 
-                    if separate_feeders:
-                        output_folder /= model_map.feeder
-                        output_redirect /= model_map.feeder
-                        output_folder.mkdir(exist_ok=True)
+                else:
+                    output_folder.mkdir(exist_ok=True)
 
-                    with open(output_folder / model_map.opendss_file, "a") as fp:
-                        fp.write(dss_string)
+                if separate_feeders:
+                    output_folder /= model_map.feeder
+                    output_redirect /= model_map.feeder
+                    output_folder.mkdir(exist_ok=True)
 
-                    if separate_substations and separate_feeders:
-                        substations_redirect[model_map.substation].append(Path(model_map.feeder) / model_map.opendss_file)
+                with open(output_folder / model_map.opendss_file, "a", encoding="utf-8") as fp:
+                    fp.write(dss_string)
 
-                    elif separate_substations:
-                        substations_redirect[model_map.substation].append(Path(model_map.opendss_file))
+                if separate_substations and separate_feeders:
+                    substations_redirect[model_map.substation].append(
+                        Path(model_map.feeder) / model_map.opendss_file
+                    )
 
-                    if separate_feeders:
-                        combined_feeder_sub = Path(model_map.substation) / Path(model_map.feeder)
-                        if combined_feeder_sub not in feeders_redirect:
-                            feeders_redirect[combined_feeder_sub] = []
-                        feeders_redirect[combined_feeder_sub].append(Path(model_map.opendss_file))
+                elif separate_substations:
+                    substations_redirect[model_map.substation].append(Path(model_map.opendss_file))
 
-                    files_to_redirect.append(output_redirect / model_map.opendss_file)
+                if separate_feeders:
+                    combined_feeder_sub = Path(model_map.substation) / Path(model_map.feeder)
+                    if combined_feeder_sub not in feeders_redirect:
+                        feeders_redirect[combined_feeder_sub] = []
+                    feeders_redirect[combined_feeder_sub].append(Path(model_map.opendss_file))
 
-                    
-            else:
-                print(f'Mapper {mapper_name} not found. Skipping')
-
-            #TODO: Set write_master for base and substation/feeder subclasses using this methodology: https://github.com/NREL/ditto/blob/master/ditto/writers/opendss/write.py#L3865
-
-        
+                files_to_redirect.append(output_redirect / model_map.opendss_file)
--- tests/opendss/test_opendss_writer.py
+++ tests/opendss/test_opendss_writer.py
@@ -1,53 +1,31 @@
 """ Module for testing writers."""
-from pathlib import Path
 
+from gdm import (
+    DistributionBus,
+    SequenceImpedanceBranch,
+    MatrixImpedanceBranch,
+    GeometryBranch,
+    DistributionCapacitor,
+    DistributionTransformer,
+)
+from infrasys.system import System
 import pytest
 
 from ditto.writers.opendss.write import Writer
-from gdm import DistributionBus, SequenceImpedanceBranch, SequenceImpedanceBranchEquipment, MatrixImpedanceBranch, GeometryBranch, DistributionCapacitor, DistributionTransformer
-from infrasys.system import System
+
+MODULES = [
+    DistributionBus,
+    SequenceImpedanceBranch,
+    MatrixImpedanceBranch,
+    GeometryBranch,
+    DistributionCapacitor,
+    DistributionTransformer,
+]
 
 
-def test_bus():
-    test_bus = DistributionBus.example()
-    system = System(name='test bus', auto_add_composed_components=True)
-    system.add_component(test_bus)
+@pytest.mark.parametrize("component", MODULES)
+def test_component(component):
+    system = System(name=f"test {component.__name__}", auto_add_composed_components=True)
+    system.add_component(component.example())
     writer = Writer(system)
-    writer.write(separate_substations=False,separate_feeders=False)
-
-def test_sequence_impedance_branch():
-    test_branch = SequenceImpedanceBranch.example()
-    system = System(name='test sequence impedance', auto_add_composed_components=True)
-    system.add_component(test_branch)
-    writer = Writer(system)
-    writer.write(separate_substations=False,separate_feeders=False)
-
-def test_matrix_impedance_branch():
-    test_branch = MatrixImpedanceBranch.example()
-    system = System(name='test matrix impedance', auto_add_composed_components=True)
-    system.add_component(test_branch)
-    writer = Writer(system)
-    writer.write(separate_substations=False,separate_feeders=False)
-
-def test_geometry_branch():
-    test_branch3 = GeometryBranch.example()
-    system = System(name='test geometry branch', auto_add_composed_components=True)
-    system.add_component(test_branch)
-    writer = Writer(system)
-    writer.write(separate_substations=False,separate_feeders=False)
-
-def test_capacitor():
-    test_capacitor = DistributionCapacitor.example()
-    system = System(name='test capacitor', auto_add_composed_components=True)
-    system.add_component(test_capacitor)
-    writer = Writer(system)
-    writer.write(separate_substations=False,separate_feeders=False)
-
-def test_transformer():
-    test_transformer = DistributionTransformer.example()
-    system = System(name='test transformer', auto_add_composed_components=True)
-    system.add_component(test_transformer)
-    writer = Writer(system)
-    writer.write(separate_substations=False,separate_feeders=False)
-
-
+    writer.write(separate_substations=False, separate_feeders=False)

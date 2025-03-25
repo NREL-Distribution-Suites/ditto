a2f7351e394759fa97259c3777997a3e9da27ca5
tarekelgindy <tarek.elgindy@gmail.com>
2025-03-25 16:47:15 -0600

    untested changes to read from load file not network

--- src/ditto/readers/cyme/equipment/load_equipment.py
+++ src/ditto/readers/cyme/equipment/load_equipment.py
@@ -3,16 +3,18 @@ from ditto.readers.cyme.cyme_mapper import CymeMapper
 from gdm.distribution.equipment.load_equipment import LoadEquipment
 from gdm.distribution.equipment.phase_load_equipment import PhaseLoadEquipment
 from gdm.quantities import ActivePower, ReactivePower
+from gdm.distribution.distribution_enum import ConnectionType
 
 class LoadEquipmentMapper(CymeMapper):
     def __init__(self, system):
         super().__init__(system)
 
-    cyme_file = 'Network'
-    cyme_section = 'LOAD EQUIVALENT'
+    cyme_file = 'Load'
+    cyme_section = 'LOADS'
 
     def parse(self, row):
         name = self.map_name(row)
+        connection_type = self.map_connection_type(row)
         phase_loads = self.map_phase_loads(row)
         return LoadEquipment(name=name,
                              phase_loads=phase_loads)
@@ -20,24 +22,42 @@ class LoadEquipmentMapper(CymeMapper):
     def map_name(self, row):
         return row['LoadModelName']
 
-    def map_phase_loads(self,row):
+    def map_connection_type(self, row):
+        connection_number = int(row['Connection'])
+        connection_map = {
+            0: ConnectionType.STAR, #Yg
+            1: ConnectionType.STAR, #Y
+            2: ConnectionType.DELTA, #Delta
+            3: ConnectionType.OPEN_DELTA, #Open Delta
+            4: ConnectinType.DELTA, #Closed Delta
+            5: ConnectionType.ZIG_ZAG, # Zg
+            6: ConnectionType.STAR, # CT
+            7: ConnectionType.DELTA, # Dg - Not sure what this is?
+        }
+        return connection_map[connection_number]
+
+    def map_phase_loads(self, row):
+        # Get the PhaseLoadEquipment with the same name as the Load
+        name = row['DeviceNumber']
         phase_loads = []
-        for phase in range(1,4):
-            mapper = PhaseLoadEquipmentMapper(self.system)
-            phase_load = mapper.parse(row, phase)
-            phase_loads.append(phase_load)
+        for phase in ['A', 'B', 'C']:
+            phase_load_name = name + '_' + phase
+            phase_load = self.system.get_component(component_type=PhaseLoadEquipment, name=phase_load_name)
+            if phase_load is not None:
+                phase_loads.append(phase_load)
         return phase_loads
 
 class PhaseLoadEquipmentMapper(CymeMapper):
     def __init__(self, system):
         super().__init__(system)
 
-    cyme_file = 'Network'  
-    cyme_section = "LOAD EQUIVALENT"
+    cyme_file = 'Load'  
+    cyme_section = "CUSTOMER LOADS"
 
-    def parse(self, row, phase):
-        name = self.map_name(row, phase)
-        real_power, reactive_power = self.map_powers(row, phase)
+    def parse(self, row):
+        name = self.map_name(row)
+        real_power = map_real_power(row),
+        reactive_power = map_reactive_power(row),
         z_real = self.map_z_real(row)
         z_imag = self.map_z_imag(row)
         i_real = self.map_i_real(row)
@@ -54,81 +74,43 @@ class PhaseLoadEquipmentMapper(CymeMapper):
                                   p_real=p_real,
                                   p_imag=p_imag)
 
-    def map_name(self, row, phase):
-        if phase == 1:
-            return row['NodeID'] + "_A"
-        if phase == 2:
-            return row['NodeID'] + "_B"
-        if phase == 3:
-            return row['NodeID'] + "_C"
+    def map_name(self, row):
+        phase = row['LoadPhase']
+        return row['DeviceNumber']+"_"+str(phase)
         
-    def map_real_power(self, row, phase):
-        if phase == 1:
-            kW = float(row["Value1A"])
-        elif phase == 2:
-            kW = float(row["Value1B"])
-        elif phase == 3:
-            kW = float(row["Value1C"])
-        return ActivePower(kW, 'kilowatt')
-    
-    def map_reactive_power(self, row, phase):
-        if phase == 1:
-            kvar = float(row["Value2A"])
-        elif phase == 2:
-            kvar = float(row["Value2B"])
-        elif phase == 3:
-            kvar = float(row["Value2C"])
+    def compute_powers(self, row):
+        v1 = float(row['Value1'])
+        v2 = float(row['Value2'])
+        kw = None
+        kvar = None
+        value_type = int(row['ValueType'])
+        # kw and kvar
+        if value_type == 0:
+            kw = v1
+            kvar = v2
+        # kva and pf
+        elif value_type == 1:
+            kw = v1 * v2
+            kvar = v1 * (1 - v2**2) ** 0.5
+        # kw and pf
+        elif value_type == 2:
+            kw = v1
+            kvar = v1 * (1/v2**2 - 1) ** 0.5
+        # amp and pf
+        else:
+            pass
+        return ActivePower(kw, 'kilowatt'), ReactivePower(kvar, 'kilovar')
+
+
+    def map_real_power(self, row):
+        kw, kvar = self.compute_powers(row)
+        return ActivePower(kw, 'kilowatt')
+
+    def map_reactive_power(self, row):
+        kw, kvar = self.compute_powers(row)
         return ReactivePower(kvar, 'kilovar')
     
-    def map_kva_pf(self, row, phase):
-        if phase == 1:
-            kw = float(row["Value1A"]) * float(row["Value2A"])
-            kvar = float(row["Value1A"]) * (1 - float(row["Value2A"])**2) ** 0.5
-        elif phase == 2:
-            kw = float(row["Value1B"]) * float(row["Value2B"])
-            kvar = float(row["Value1B"]) * (1 - float(row["Value2B"])**2) ** 0.5
-        elif phase == 3:
-            kw = float(row["Value1C"]) * float(row["Value2C"])
-            kvar = float(row["Value1C"]) * (1 - float(row["Value2C"])**2) ** 0.5
-        return ActivePower(kw, 'kilowatt'), ReactivePower(kvar, 'kilovar')
-
-    def map_kw_pf(self, row, phase):
-        if phase == 1:
-            kw = float(row["Value1A"])
-            
-            if float(row["Value2A"]) <= 0 or float(row["Value2A"]) > 1:
-                logger.warning("Power factor must be between 0 and 1 (exclusive of 0). Setting kVAR to 0.")
-                kvar = 0
-            else:
-                kvar = float(row["Value1A"]) * ((1 / float(row["Value2A"])**2) - 1) ** 0.5
-        elif phase == 2:
-            kw = float(row["Value1B"])
-            
-            if float(row["Value2B"]) <= 0 or float(row["Value2B"]) > 1:
-                logger.warning("Power factor must be between 0 and 1 (exclusive of 0). Setting kVAR to 0.")
-                kvar = 0
-            else:
-                kvar = float(row["Value1B"]) * ((1 / float(row["Value2B"])**2) - 1) ** 0.5
-        elif phase == 3:
-            kw = float(row["Value1C"])
-            
-            if float(row["Value2C"]) <= 0 or float(row["Value2C"]) > 1:
-                logger.warning("Power factor must be between 0 and 1 (exclusive of 0). Setting kVAR to 0.")
-                kvar = 0
-            else:
-                kvar = float(row["Value1C"]) * ((1 / float(row["Value2C"])**2) - 1) ** 0.5
-        return ActivePower(kw, 'kilowatt'), ReactivePower(kvar, 'kilovar')
-
-    def map_powers(self, row, phase):
-        if row['Format'] == 'KW_KVAR':
-            return self.map_real_power(row, phase), self.map_reactive_power(row, phase)
-        elif row['Format'] == 'KVA_PF':
-            return self.map_kva_pf(row, phase)
-        elif row['Format'] == 'KW_PF':
-            return self.map_kw_pf(row, phase)
-        elif row['Format'] == 'AMP_PF':
-            pass #TODO
-
+    # Is this included in CYME 9.* ? It was in customer class in previous cyme versions
     def map_z_real(self, row):
         return 1
 
